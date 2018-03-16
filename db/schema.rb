de60965e3966402fdafe636b03cce8c09a4691a5
gauzeee <gauzeee@gmail.com>
2018-03-16 11:20:19 +0300

    Lesson3Try2

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315172155) do
+ActiveRecord::Schema.define(version: 20180316081907) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "correct", default: false
+    t.boolean "correct", default: false, null: false
+    t.integer "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -28,17 +29,19 @@ ActiveRecord::Schema.define(version: 20180315172155) do
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "test_id"
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0
-    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "categorie_id"
+    t.index ["categorie_id"], name: "index_tests_on_categorie_id"
   end
 
   create_table "users", force: :cascade do |t|
