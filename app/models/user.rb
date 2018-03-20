a3a45ede4c3d57f247536af45d9a0851165d74d1
gauzeee <gauzeee@gmail.com>
2018-03-20 21:31:49 +0300

    Lesson4Try2.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  def show_my_tests(level)
+  def tests_by_level(level)
     Test.joins('JOIN test_passages
     ON tests.id = test_passages.test_id').
     where('test_passages.user_id = :user_id AND level = :level',
