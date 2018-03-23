603b16688f1abcad22c0384b02c4611ae35a2054
gauzeee <gauzeee@gmail.com>
2018-03-23 13:43:36 +0300

    Lesson6Try1

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  scope :correct, -> { where(correct: true) }
+
+  validates :body, presence: true
+  validate :quantity
+
+  private
+
+  def quantity
+    errors.add(:question) if question.answers.size > 4
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,10 +5,14 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User", foreign_key: "author_id"
 
-  def self.title_by_category(category_title)
-    Test.joins('JOIN categories
-    ON tests.category_id = categories.id').
-    where(categories: { title: category_title }).
-    order('tests.title DESC').pluck(:title)
-  end
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :title_by_category, -> (category_title) {
+    joins(:category).where(categories: { title: category_title }).
+    order(title: :desc).pluck(:title) }
+
+  validates :title, presence: true, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 0 }
 end
