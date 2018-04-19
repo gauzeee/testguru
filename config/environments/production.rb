4436fcdba0387494ce68144beabe677f3cb79b02
gauzeee <gauzeee@gmail.com>
2018-04-19 14:31:16 +0300

    L17:config_action_mailer_fix1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'still-mountain-58180.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
