a69cd1677ad9abbc083d287f99b5fc54697c9812
gauzeee <gauzeee@gmail.com>
2018-04-19 16:20:39 +0300

    L17:fix_key_test_passages

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   has_many :questions
-  has_many :test_passages, **dependent: :nullify**
+  has_many :test_passages
   has_many :users, through: :test_passages
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: :author_id
