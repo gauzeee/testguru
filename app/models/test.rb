64c8ed77e10b8953d6acf1af0bd8b2aa2f631ac7
gauzeee <gauzeee@gmail.com>
2018-04-19 17:00:56 +0300

    L17:fix_key_questions

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
-  has_many :questions
+  has_many :questions, dependent: :destroy
   has_many :test_passages
   has_many :users, through: :test_passages
   belongs_to :category
