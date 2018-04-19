79d1c171c0c7cd48dc2a6f8061efba4aeea0bb74
gauzeee <gauzeee@gmail.com>
2018-04-19 17:17:33 +0300

    L17:fix_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,9 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: 'gmail.com',
+    address: "smtp.mail.ru",
+    port: 465,
+    domain: 'mail.ru',
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
