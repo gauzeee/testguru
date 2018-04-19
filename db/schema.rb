56c95d288f8d339503f08eeec6e3af6f53ee1b17
gauzeee <gauzeee@gmail.com>
2018-04-19 16:08:15 +0300

    L17:fix_key_test_passages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418092026) do
+ActiveRecord::Schema.define(version: 20180419130302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,4 +105,5 @@ ActiveRecord::Schema.define(version: 20180418092026) do
     t.index ["type"], name: "index_users_on_type"
   end
 
+  add_foreign_key "test_passages", "tests", on_delete: :cascade
 end
