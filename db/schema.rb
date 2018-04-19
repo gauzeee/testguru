4bf9ba230cc574417975c27d2fb9f0d662a99af7
gauzeee <gauzeee@gmail.com>
2018-04-19 16:44:32 +0300

    L17:fix_key_questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419130302) do
+ActiveRecord::Schema.define(version: 20180419132443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,5 +105,6 @@ ActiveRecord::Schema.define(version: 20180419130302) do
     t.index ["type"], name: "index_users_on_type"
   end
 
+  add_foreign_key "questions", "tests", on_delete: :cascade
   add_foreign_key "test_passages", "tests", on_delete: :cascade
 end
